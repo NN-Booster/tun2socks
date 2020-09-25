1cb2234d1486dff469709bd82728ade4a4bd724f
Yuko Zheng <hi@yuko.me>
2020-09-25 12:15:02 +0800

    Update debug.h

--- lwip/lwip/debug.h
+++ lwip/lwip/debug.h
@@ -62,12 +62,7 @@
 /** flag for LWIP_DEBUGF to halt after printing this debug message */
 #define LWIP_DBG_HALT          0x08U
 
-#ifndef LWIP_NOASSERT
-#define LWIP_ASSERT(message, assertion) do { if(!(assertion)) \
-  LWIP_PLATFORM_ASSERT(message); } while(0)
-#else  /* LWIP_NOASSERT */
 #define LWIP_ASSERT(message, assertion) 
-#endif /* LWIP_NOASSERT */
 
 /** if "expression" isn't true, then print "message" and execute "handler" expression */
 #ifndef LWIP_ERROR
